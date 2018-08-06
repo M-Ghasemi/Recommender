c89e8587a9e83b1b935fa67954c2c615fb5d64ee
Mohammad <hamp.cpas@gmail.com>
2018-08-06 16:19:56 +0430

    add settings file

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+MONGO = {
+    'HOST': 'localhost',
+    'DB': 'DataMining',
+    'PORT': 27017,
+    'USER': '',
+    'PASSWORD': ''
+}
